e02afa65bbfd7f3961e984e47ad3d03b75705860
valerisuleo <valerio.risuleo@gmail.com>
2023-12-13 16:18:26 +0000

    fix dep issues

---
+++ docs/precache-manifest.319945e73a6c85934ce33ff8ee513518.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105773cca01b70e1331e2c59e73097e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdc7df394effa4d904c9",
+    "url": "/static/css/2.2bb34ba5.chunk.css"
+  },
+  {
+    "revision": "192398f95c61c6639b74",
+    "url": "/static/css/main.bc436e89.chunk.css"
+  },
+  {
+    "revision": "fdc7df394effa4d904c9",
+    "url": "/static/js/2.b5979f9c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b5979f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192398f95c61c6639b74",
+    "url": "/static/js/main.499f3ea1.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "daa84cabab3e01002d16d35a53531fee",
+    "url": "/static/media/about.daa84cab.jpg"
+  },
+  {
+    "revision": "4a2e7d6109b3e6f232bae80fd26a09d4",
+    "url": "/static/media/beat.4a2e7d61.mp3"
+  },
+  {
+    "revision": "23a0ae3ff657da0e8c30d6953b61672e",
+    "url": "/static/media/click.23a0ae3f.wav"
+  },
+  {
+    "revision": "e3c041076d8924192a85ad396ba9eb76",
+    "url": "/static/media/cursor.e3c04107.wav"
+  },
+  {
+    "revision": "effeb8ccc7b5cd4c3d19e00dca8927d7",
+    "url": "/static/media/darkskies.effeb8cc.jpg"
+  },
+  {
+    "revision": "074b2beafda60116b00aec2791b5cb21",
+    "url": "/static/media/fatto.074b2bea.jpg"
+  },
+  {
+    "revision": "95d10f502e973b4a79dae1e1882f623e",
+    "url": "/static/media/fingerprint.95d10f50.png"
+  },
+  {
+    "revision": "a50cf897c40c543948841eb044ed601e",
+    "url": "/static/media/hero.a50cf897.jpg"
+  },
+  {
+    "revision": "2c8f540ef1666d481f1b0e5be20fb1b9",
+    "url": "/static/media/itunesartwork.2c8f540e.jpg"
+  },
+  {
+    "revision": "1b9322de09feaf826936cc51062de623",
+    "url": "/static/media/mtvcribs.1b9322de.jpg"
+  },
+  {
+    "revision": "5bfcd85b3f1a7d54808d3e939b4de9f0",
+    "url": "/static/media/openweather.5bfcd85b.jpg"
+  },
+  {
+    "revision": "5acf5904636dc805d6da452c7bb3ef00",
+    "url": "/static/media/pirlapiccolo.5acf5904.jpg"
+  },
+  {
+    "revision": "cc8c802f04e9899e085b3fc9b7037389",
+    "url": "/static/media/player.cc8c802f.gif"
+  },
+  {
+    "revision": "d38cc517555fd486c42101ddf4d61c9d",
+    "url": "/static/media/speakeasy.d38cc517.jpg"
+  },
+  {
+    "revision": "09b1c38fb9644289a9aa10f62c1bbb2f",
+    "url": "/static/media/stb.09b1c38f.jpg"
+  },
+  {
+    "revision": "d833ba89eb2e0a5e2ab8feb77860de0f",
+    "url": "/static/media/stealmydeck.d833ba89.jpg"
+  },
+  {
+    "revision": "917ed14ea22a5fe415d78bbccebdbe9a",
+    "url": "/static/media/udemy.917ed14e.jpg"
+  },
+  {
+    "revision": "67db3ce666e723f3aae7bbbf5317c5b4",
+    "url": "/static/media/universitadeglistudidiparma.67db3ce6.jpg"
+  },
+  {
+    "revision": "c03bce99649604bfb50054e1d659bd55",
+    "url": "/static/media/universitadeglistudidiromatre.c03bce99.jpg"
+  },
+  {
+    "revision": "728458ef8501bada62e87c060f49fa55",
+    "url": "/static/media/wholetthedogsout.728458ef.jpg"
+  },
+  {
+    "revision": "fec29c46dff0df3eaeab8660a8933ed0",
+    "url": "/static/media/won.fec29c46.gif"
+  }
+]);
